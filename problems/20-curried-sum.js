07a52e12d14745cc7dc93964b3ddfe915b20de84
Nikolas (Daniel) Vincenti <165183152+NikVince@users.noreply.github.com>
2025-06-01 08:23:55 +0200

    UPDATED problem '20' with implementation that is passing tests.

--- problems/20-curried-sum.js
+++ problems/20-curried-sum.js
@@ -44,6 +44,19 @@ AFTER YOU ARE FINISHED WITH THIS PROBLEM, ASK FOR A CODE REVIEW
 
 function curriedSum(numArgs) {
   // Your code here
+  let numbers = [];
+
+  function _curriedSum(num) {
+    numbers.push(num);
+    
+    if (numbers.length === numArgs) {
+      return numbers.reduce((sum, n) => sum + n, 0);
+    } else {
+      return _curriedSum;
+    }
+  }
+  
+  return _curriedSum;
 }
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
