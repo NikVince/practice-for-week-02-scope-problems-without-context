2372ccca72da37cfc1e5355aec80f846eba86f7b
Nikolas (Daniel) Vincenti <165183152+NikVince@users.noreply.github.com>
2025-06-01 08:15:51 +0200

    UPDATED problem '19' with implementation that is passing tests.

--- problems/19-coin-collector.js
+++ problems/19-coin-collector.js
@@ -25,7 +25,17 @@ Example 3:
 
 ***********************************************************************/
 function coinCollector(numCoins) {
-  // Your code here
+  let coins = [];
+  
+  return function(coin) {
+    coins.push(coin);
+    
+    if (coins.length === numCoins) {
+      return coins;
+    } else {
+      return arguments.callee; // Return the same function for chaining
+    }
+  };
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
