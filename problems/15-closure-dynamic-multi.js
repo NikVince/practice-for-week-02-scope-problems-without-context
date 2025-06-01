f8ff64feb36869682a85f13d0d65e379789de598
Nikolas (Daniel) Vincenti <165183152+NikVince@users.noreply.github.com>
2025-06-01 07:47:03 +0200

    UPDATED problem '15' with implementation that is passing tests.

--- problems/15-closure-dynamic-multi.js
+++ problems/15-closure-dynamic-multi.js
@@ -20,6 +20,9 @@ multiplyByFive(5); // returns 25
 
 function dynamicMultiply(num) {
   // Your code here
+  return function(value) {
+    return value * num;
+  }
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
